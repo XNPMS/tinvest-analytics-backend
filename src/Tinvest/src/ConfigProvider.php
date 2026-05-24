d1912b80804ee87f2014a4dd2761b26b08f2bffd
fcritic <fcritic@users.noreply.github.com>
2026-05-24 21:42:41 +0300

    tinvest: обновлён ConfigProvider

--- src/Tinvest/src/ConfigProvider.php
+++ src/Tinvest/src/ConfigProvider.php
@@ -4,18 +4,23 @@ declare(strict_types=1);
 
 namespace Tinvest;
 
+use System\Factory\HandlerDelegatorFactory;
 use System\Factory\InputFilterAbstractFactory;
+use Tinvest\Factory\BrokerTokenServiceFactory;
+use Tinvest\Handler\AssetAllocationHandler;
+use Tinvest\Handler\DashboardSummaryHandler;
+use Tinvest\Handler\InstrumentsPerformanceHandler;
+use Tinvest\Handler\OnboardingAccountsHandler;
+use Tinvest\Handler\OnboardingTokenHandler;
+use Tinvest\Handler\PortfolioHistoryHandler;
+use Tinvest\Handler\RevokeTokenHandler;
+use Tinvest\InputFilter\PortfolioHistoryInputFilter;
 use Tinvest\InputFilter\TinvestAccountIdsInputFilter;
 use Tinvest\InputFilter\TinvestTokenInputFilter;
+use Tinvest\Service\BrokerTokenService;
 
 class ConfigProvider
 {
-    /**
-     * Returns the configuration array
-     *
-     * To add a bit of a structure, each section is defined in a separate
-     * method which returns an array with its configuration.
-     */
     public function __invoke(): array
     {
         return [
@@ -23,21 +28,40 @@ class ConfigProvider
         ];
     }
 
-    /**
-     * Returns the container dependencies
-     */
     public function getDependencies(): array
     {
         return [
-            'factories' => $this->getFactories(),
+            'factories' => array_merge($this->getFactories(), $this->getFactoriesInputFilters()),
+            'delegators' => $this->getDelegators(),
         ];
     }
 
     private function getFactories(): array
     {
         return [
+            BrokerTokenService::class => BrokerTokenServiceFactory::class,
+        ];
+    }
+
+    private function getFactoriesInputFilters(): array
+    {
+        return [
             TinvestTokenInputFilter::class => InputFilterAbstractFactory::class,
             TinvestAccountIdsInputFilter::class => InputFilterAbstractFactory::class,
+            PortfolioHistoryInputFilter::class => InputFilterAbstractFactory::class,
+        ];
+    }
+
+    private function getDelegators(): array
+    {
+        return [
+            AssetAllocationHandler::class => [HandlerDelegatorFactory::class],
+            DashboardSummaryHandler::class => [HandlerDelegatorFactory::class],
+            OnboardingAccountsHandler::class => [HandlerDelegatorFactory::class],
+            OnboardingTokenHandler::class => [HandlerDelegatorFactory::class],
+            InstrumentsPerformanceHandler::class => [HandlerDelegatorFactory::class],
+            PortfolioHistoryHandler::class => [HandlerDelegatorFactory::class],
+            RevokeTokenHandler::class => [HandlerDelegatorFactory::class],
         ];
     }
 }
